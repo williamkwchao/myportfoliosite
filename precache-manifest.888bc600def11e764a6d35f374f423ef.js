6daf38e0e319752f3a83069a9b39a20c07864aff
William Chao <williamchao@utexas.edu>
2020-05-26 17:51:01 -0500

    Updates

---
+++ precache-manifest.888bc600def11e764a6d35f374f423ef.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8561adeea28287db559729a9bba8cd2",
+    "url": "/myportfoliosite/index.html"
+  },
+  {
+    "revision": "2e974bb573f4520599ed",
+    "url": "/myportfoliosite/static/css/2.def00553.chunk.css"
+  },
+  {
+    "revision": "7e8c0c8e669f16857c1d",
+    "url": "/myportfoliosite/static/css/main.16cd3934.chunk.css"
+  },
+  {
+    "revision": "2e974bb573f4520599ed",
+    "url": "/myportfoliosite/static/js/2.6e76b3b6.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/myportfoliosite/static/js/2.6e76b3b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e8c0c8e669f16857c1d",
+    "url": "/myportfoliosite/static/js/main.ec63fa16.chunk.js"
+  },
+  {
+    "revision": "a2fa936631a8ec1deaeb",
+    "url": "/myportfoliosite/static/js/runtime-main.522f49bd.js"
+  }
+]);
